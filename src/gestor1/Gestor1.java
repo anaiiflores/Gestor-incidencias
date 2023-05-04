b80a3a7d5109eacab3b652dd252a7f7e2670c19e
Anai <anaiflo2003@gmail.com>
2023-05-04 10:49:36 +0200

    Inicio

---
+++ src/gestor1/Gestor1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestor1;
+
+/**
+ *
+ * @author 34633
+ */
+public class Gestor1 {
+
+   
+    
+    public void inicio(){
+        System.out.println("Hola");
+       
+
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola");
+        Gestor1 gestorr = new Gestor1();
+        gestorr.inicio();
+    }
+    
+}
